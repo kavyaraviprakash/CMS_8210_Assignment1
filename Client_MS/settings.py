2c334e2dc4fa614d45e05cd5d29070538af8d576
kavyaraviprakash <70050485+kavyaraviprakash@users.noreply.github.com>
2021-01-31 21:29:35 -0600

    Making changes to static file

--- Client_MS/settings.py
+++ Client_MS/settings.py
@@ -128,14 +128,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL ='/media/'
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
